92b4b5e85504d6b0396e9d60ce3072647ae69518
Shivank Mathur <shivumat@gmail.com>
2021-04-28 23:46:22 +0530

    Image upload done

--- src/component/CustomUmbrella.jsx
+++ src/component/CustomUmbrella.jsx
@@ -14,13 +14,19 @@ const CustomUmbrella = (props) => {
             width : 60,
             marginLeft : '45%',
             marginTop : '15%',
+        },
+        customImage : {
+            width: 50, 
+            right: '33%',
+            bottom: '25px',
+            position: 'relative'
         }
     }
 
     return <div>
         {isLoading ? <img className='loading' style={styles.loading} src={colors[colorType].loader}/> : 
         <img style={styles.umbrella} src={colors[colorType].umbrella}/>}
-        {customImage && <img src={customImage} style={{width: 50, height: 50}}/>}
+        {customImage && <img src={customImage} style={styles.customImage}/>}
     </div>
 }
 
